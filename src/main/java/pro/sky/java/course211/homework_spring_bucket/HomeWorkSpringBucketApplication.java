561b29a7f1269a5366a321d55097aa60710a1335
RickLyudmila <Lrick@yandex.ru>
2022-01-14 20:18:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course211/homework_spring_bucket/HomeWorkSpringBucketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course211.homework_spring_bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpringBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkSpringBucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course211/homework_spring_bucket/HomeWorkSpringBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course211.homework_spring_bucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkSpringBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
